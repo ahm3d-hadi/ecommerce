b551b0a636bca18a26708c3956ebb7603dbca79b
Ahmed Elhadi <hadiiae96@gmail.com>
2020-11-30 12:24:00 +0400

    render cart data

--- ajw/store/views.py
+++ ajw/store/views.py
@@ -11,8 +11,15 @@ def store(request):
 
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {'items': items}
+    return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
